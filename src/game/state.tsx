4745b90c61c027573cf30a5da259cc1c8b2dfc50
ifeelagood <thicccatto3@gmail.com>
2025-10-04 14:57:03 +1000

    fetch csv from state

--- src/game/state.tsx
+++ src/game/state.tsx
@@ -2,10 +2,20 @@ import { EventGraph } from "./event.js"
 import type { NodeId } from "./event.js"
 import { Resources }  from "./resource.js"
 
+
+const eventCsvContent = await fetch("/events.csv").then(res => res.text());
+const optionsCsvContent = await fetch("/options.csv").then(res => res.text())
+  
 class GameState {
 
   public eventGraph: EventGraph|undefined = undefined;
   public resources: Resources = new Resources();
   public currentNode: NodeId = "start";
 
+
+  constructor() {
+    this.eventGraph = new EventGraph(eventCsvContent, optionsCsvContent);
+
+
+  }
 }
